ce3b8554cf657516f9abd1c6397906f2cd5faee8
qureshiowaisdigitalks <owais@digitalks.co.in>
2025-12-18 16:27:02 +0530

    issue fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,32 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import { LayoutComponent } from '../layout/layout.component';
-import { AuthGuard } from '../core/auth/auth.guard.ts.service';
 import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 import { UsersComponent } from '../pages/users/users.component';
+import { AuthGuard } from '../core/auth/auth.guard.ts.service';
+import { ReportsComponent } from '../pages/reports/reports.component';
+import { SettingsComponent } from '../pages/settings/settings.component';
 
 
-// app-routing.module.ts
 export const routes: Routes = [
+  // Public
   { path: 'login', component: LoginComponent },
 
+  // Protected App
   {
     path: '',
     component: LayoutComponent,
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'users', component: UsersComponent },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-    ]
-  }
+      { path: 'reports', component: ReportsComponent },
+      { path: 'settings', component: SettingsComponent },
+
+      // default
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+
+  // Fallback
+  { path: '**', redirectTo: 'dashboard' },
 ];
--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 
+  constructor(private router: Router) {}
+
+  navigate(path: string) {
+    this.router.navigate([path]);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
