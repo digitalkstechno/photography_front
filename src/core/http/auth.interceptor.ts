61aed245079ab4e4e9d74a7b2b50b57f1e2a2768
qureshiowaisdigitalks <owais@digitalks.co.in>
2025-12-24 12:32:15 +0530

    add appointments service and dashboard integration

--- src/core/http/auth.interceptor.ts
+++ src/core/http/auth.interceptor.ts
@@ -1,15 +1,14 @@
 // core/http/auth.interceptor.ts
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const token = localStorage.getItem('token');
+// @Injectable()
+// export class AuthInterceptor implements HttpInterceptor {
+//   intercept(req: HttpRequest<any>, next: HttpHandler) {
+//     const token = localStorage.getItem('token');
 
-    if (token) {
-      req = req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` }
-      });
-    }
-    return next.handle(req);
-  }
-}
-  
+//     if (token) {
+//       req = req.clone({
+//         setHeaders: { Authorization: `Bearer ${token}` }
+//       });
+//     }
+//     return next.handle(req);
+//   }
+// }
--- src/pages/dashboard/dashboard.component.ts
+++ src/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,35 @@
+import { AppointmentsService } from './../../core/appoinments/appoinments.service';
 import { Component } from '@angular/core';
+import { TableComponent } from "../../shared/components/table/table.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [TableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  constructor(private appointmentService: AppointmentsService) { }
+
+  // Common columns for all appointment tables
+  appointmentColumns = [
+    { key: 'patient.name', label: 'Patient' },
+    { key: 'doctor.name', label: 'Doctor' },
+    { key: 'appointmentDate', label: 'Date' },
+    { key: 'timeSlot.startTime', label: 'From' },
+    { key: 'timeSlot.endTime', label: 'To' },
+    { key: 'status', label: 'Status' },
+  ];
+
+  // Generic fetch function (status-based)
+  fetchByStatus = (status: string) => {
+    return (params: any) => {
+      return this.appointmentService.getAppointments({
+        ...params,
+        status
+      }).toPromise();
+    };
+  };
 }
