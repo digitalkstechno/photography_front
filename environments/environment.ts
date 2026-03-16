c0fded81e7a45dbe62e283ba337b5923c945f64a
qureshiowaisdigitalks <owais@digitalks.co.in>
2026-03-16 16:46:55 +0530

    true

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://photography-back-ajwk.onrender.com'
 };
